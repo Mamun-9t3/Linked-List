11e9e055216950b03843cd94bd16d17e65f92140
Mamun-9t3 <mamunalam923@outlook.com>
2026-09-16 23:34:39 +0530

    Doubly -Insertion,-Deletion

---
+++ Doubly_DeleteAnyPos.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *link;
+    struct node* prev;
+};
+
+void add_to_list(struct node** head,int data){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->prev= NULL;
+    temp->data = data;
+    temp->link= NULL;
+
+    *head = temp;
+    
+};
+
+void add_at_beg(struct node** head, int data){
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->data= data;
+    temp->link = *head;
+
+    (*head)->prev = temp;
+    (*head) = temp;
+}
+
+void add_at_end(struct node**head, int data){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->data = data;
+    temp->link = NULL;
+    struct node *ptr = NULL;
+    ptr= *head;
+
+    while(ptr->link != NULL){
+        ptr= ptr->link;
+    };
+    temp->prev = ptr;
+    ptr->link = temp;
+};
+
+void insert_between(struct node** head,int data, int pos){
+    struct node* temp  =(struct node*)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->data = data;
+    temp->link = NULL;
+
+    struct node* ptr = NULL;
+    struct node* ptr2 = NULL;
+    ptr = *head;
+
+    while(pos > data){
+        ptr = ptr->link;
+        pos--;
+    };
+    ptr2 = ptr->link;
+    temp->prev = ptr;
+    temp-> link = ptr2;
+    ptr->link = temp;
+    ptr2->prev= temp;
+};
+
+
+void del_any_pos(struct node** head,int  position){
+    struct node * temp  = NULL;
+    struct node * temp2  = NULL;
+
+    temp = *head;
+    while(position > 1){
+        temp = temp->link;
+        position--;
+    };
+    temp2 = temp->prev;
+    temp2-> link = temp->link;
+    temp->link->prev = temp2;
+    free(temp);
+}
+
+int main(){
+struct node*head = NULL;
+add_to_list(&head, 45);
+add_at_beg(&head,56);
+add_at_end(&head, 65);
+int data = 2;
+insert_between(&head,67,data);
+del_any_pos(&head, 3);
+struct node*ptr = NULL;
+ptr= head;
+while(ptr != NULL){
+    printf("%d\n", ptr->data);
+    ptr= ptr->link;
+};
+
+return 0;
+}
